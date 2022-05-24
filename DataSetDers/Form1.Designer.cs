b8aed21e0d4b2bc366744cf4270e155e8eb2bc2d
EMRE5 <emrebendes@gmail.com>
2022-05-24 16:06:36 +0300

    Add project files.

---
+++ DataSetDers/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace DataSetDers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Ad = new System.Windows.Forms.TextBox();
+            this.Soyad = new System.Windows.Forms.TextBox();
+            this.Tel = new System.Windows.Forms.TextBox();
+            this.Eposta = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 159);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1384, 561);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.Eposta);
+            this.panel1.Controls.Add(this.Tel);
+            this.panel1.Controls.Add(this.Soyad);
+            this.panel1.Controls.Add(this.Ad);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1384, 153);
+            this.panel1.TabIndex = 1;
+            // 
+            // Ad
+            // 
+            this.Ad.Location = new System.Drawing.Point(173, 22);
+            this.Ad.Name = "Ad";
+            this.Ad.Size = new System.Drawing.Size(225, 26);
+            this.Ad.TabIndex = 0;
+            // 
+            // Soyad
+            // 
+            this.Soyad.Location = new System.Drawing.Point(173, 54);
+            this.Soyad.Name = "Soyad";
+            this.Soyad.Size = new System.Drawing.Size(225, 26);
+            this.Soyad.TabIndex = 0;
+            // 
+            // Tel
+            // 
+            this.Tel.Location = new System.Drawing.Point(173, 86);
+            this.Tel.Name = "Tel";
+            this.Tel.Size = new System.Drawing.Size(225, 26);
+            this.Tel.TabIndex = 0;
+            // 
+            // Eposta
+            // 
+            this.Eposta.Location = new System.Drawing.Point(173, 118);
+            this.Eposta.Name = "Eposta";
+            this.Eposta.Size = new System.Drawing.Size(225, 26);
+            this.Eposta.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "SoyAD";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Tel";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 20);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Eposta";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(471, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 67);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "EKLE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(594, 22);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 67);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "GÜNCELLE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(717, 22);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(117, 67);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "SİL";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1142, 114);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(199, 26);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1019, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Ad ile arama";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1384, 717);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Eposta;
+        private System.Windows.Forms.TextBox Tel;
+        private System.Windows.Forms.TextBox Soyad;
+        private System.Windows.Forms.TextBox Ad;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ DataSetDers/Form1.cs
@@ -0,0 +1,52 @@
+﻿using DataSetDers.DataSet1TableAdapters;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataSetDers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DataSet1TableAdapters.denemeTableAdapter adapter = new DataSet1TableAdapters.denemeTableAdapter();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = adapter.GetDataBy();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            adapter.InsertQuery(Ad.Text,Soyad.Text,Tel.Text,Eposta.Text);
+            foreach (Control c in panel1.Controls) if (c is TextBox) c.Text = "";
+            dataGridView1.DataSource = adapter.GetDataBy();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            adapter.UpdateQuery(Ad.Text, Soyad.Text, Tel.Text, Eposta.Text,
+                Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value.ToString()));
+            foreach (Control c in panel1.Controls) if (c is TextBox) c.Text = "";
+            dataGridView1.DataSource = adapter.GetDataBy();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            adapter.DeleteQuery(Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value.ToString()));
+            dataGridView1.DataSource = adapter.GetDataBy();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = adapter.ourSeach("%" + textBox1.Text + "%");
+        }
+    }
+}
